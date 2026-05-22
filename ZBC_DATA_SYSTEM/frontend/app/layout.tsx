4dd5bedc6938bd9d5772e82eeab4688bb22fa2a7
Namaumbo <daelo.namaumbo@gmail.com>
2026-05-22 11:55:50 +0200

    font addition

--- ZBC_DATA_SYSTEM/frontend/app/layout.tsx
+++ ZBC_DATA_SYSTEM/frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import '@fontsource-variable/figtree/wght.css';
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { QueryProvider } from "@/context/QueryProvider";
